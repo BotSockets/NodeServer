c09302d1e109d593dfdf7628fccbf0a003e0391c
callumjhays <callumjhays@gmail.com>
2016-08-13 14:46:43 +1000

    enabled cors for rapid development

--- index.js
+++ index.js
@@ -1,53 +1,89 @@
-/**
- * Created by callumjhays on 13/08/16.
- */
 var express = require('express');
+var cors = require('cors');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var fs = require('fs');
-var path = require('path');
 
-var spawn = require('child_process').spawn;
-var proc;
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+app.use(cors());
 
-app.use('/', express.static(path.join(__dirname, 'stream')));
+var users = [];
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-var sockets = {};
-
-io.on('connection', function(socket){
-  sockets[socket.id] = socket;
-  console.log("Total clients connected: ", sockets.length);
-
-  socket.on('disconnect', function(){
-    delete sockets[socket.id];
-
-    if(Object.keys(sockets).length == 0){
-      app.set('watchingFile', false);
-      if(proc) proc.kill();
-      fs.unwatchFile('./stream/image_stream.jpg');
-    }
+function create_new_user(){
+  users.push({
+    user_name: "Anonymous",
+    commands: []
   });
 
-  socket.on('start-stream', function() {
-    startStreaming(io);
-  })
+  // return the user id
+  return users.length - 1;
+}
+
+app.get('/schedule', function(req, res){
+  var time_now = new Date();
+
+  const UTC_PER_MINUTE = 60000;
+
+  var minutes_until_next_round = 4;
+
+  res.json({
+    "round_starts": new Date(time_now + minutes_until_next_round * UTC_PER_MINUTE),
+    "next_game": new Date(time_now + (180 + minutes_until_next_round) * UTC_PER_MINUTE)
+  });
 });
 
-http.listen(3000, function() {
-  console.log('listening on *:3000');
+app.get('/join', function(req, res){
+  var new_user_id = create_new_user();
+
+  res.json({
+    "user_id": new_user_id
+  });
 });
 
-function startStreaming(io) {
-  if(app.get('watchingFile')) {
-    io.sockets.emit('liveStream', 'image_stream.jpg?_t' + (Math.random() * 100000));
-    return;
+app.get('/winner', function(req, res){
+  var winner  = users[users.length - 1];
+
+  res.json({
+    "message": "This is just a test",
+
+    "user_id": users.length - 1,
+    "user_name": winner.user_name,
+    "moves_taken": winner.commands.length,
+    "commands_used": winner.commands,
+    "solved_in": 420 // hardcoded for now
+  });
+});
+
+app.post('/commands', function(req, res){
+  var resp = "";
+
+  if(req.body.user_id in users){
+    users[req.body.user_id].commands = req.body.commands;
+    resp = "confirmed (commands not yet confirmed valid)";
+  } else {
+    resp = "denied. No user with that ID found";
   }
 
-  var args = ["-w", "640", "-h", "480", "-o",
-    './stream/image_stream.jpg', '-t', '999999999', '-tl', '100'];
-}
+  res.json({
+    "submission": resp
+  });
+});
+
+app.post('/set_username', function(req, res){
+  if(req.body.user_id in users){
+    users[req.body.user_id].user_name = req.body.user_name;
+
+    res.json({
+      "username_result": "user_id " + req.body.user_id
+        + " has set their username to " + req.body.user_name
+    });
+  } else {
+    res.json({
+      "ERROR": "a user with that username does not exist"
+    });
+  }
+});
+
+app.listen(80, "0.0.0.0");
